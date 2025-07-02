f7c2050ec8ed9f9841aa46223424b00ae6ad5cdd
enzoyagui <143002331+enzoyagui@users.noreply.github.com>
2025-07-01 21:02:33 -0300

    reajustes

--- script.js
+++ script.js
@@ -25,6 +25,9 @@ function aumentaTamanho() {
 
 const campoSenha = document.querySelector('#campo-senha');
 const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const letrasMinusculas = 'abcdefghijklmnopqrstuvwxyz';
+const numeros = '0123456789';
+const simbolos = '!@%*?'
 geraSenha();
 
 function geraSenha() {
